14e00bdf131d30b9321b0afd6d110dc0ed1bca18
Leonardo Galante <lndgalante@gmail.com>
2020-05-24 19:51:20 -0300

    👌 IMPROVE: Filtering anonymous methods

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const prettier = require('prettier');
 const { generateUniqueId } = require('../lib/id');
 const { createHtmlTemplate } = require('../lib/html');
 const { createCodesandbox } = require('../lib/codesandbox');
-const { getMethods, getViewMethods, getTransactionMethods } = require('../lib/abi');
+const { filterAnonymousMethods, getMethods, getViewMethods, getTransactionMethods } = require('../lib/abi');
 
 // string helpers
 const formatHtml = (string) => prettier.format(string, { parser: 'html' });
@@ -50,7 +50,7 @@ const getInvokeElement = ({ name = '' }, { id = '' }) => `
     data-dh-property-invoke="true"
     data-dh-property-method-id="${id}"
   >
-    ${name ? name : 'anonymous'}
+    ${name}
   </button>
 `;
 
@@ -72,42 +72,38 @@ const getWeb3Tag = () => `
 `;
 
 const getHtmlFromPieces = (pieces) => pieces.map(({ html }) => html).join('<hr/>');
+
 const getHtmlFromIO = (io) => io.reduce((acc, element) => `${acc}${element[element.key]}`, '');
 
-const generateHtmlPieces = (abi = [], contractName) =>
-  abi.map((method) => {
+const generateHtmlPieces = (abi = [], contractName) => {
+  return filterAnonymousMethods(abi).map((method) => {
+    const id = generateUniqueId();
+    const { inputs = [], outputs = [], name, stateMutability } = method;
 
-    console.log("Method Name: ", method.name)
-    // anonymous functions are fall back functions. Users should not directly acces from DappHero
-    if(!method.name) return null
-      const id = generateUniqueId();
-      const { inputs = [], outputs = [], name, stateMutability } = method;
+    const isTransaction = stateMutability !== 'view';
+    const invoker = getInvokeElement(method, { id });
 
-      const isTransaction = stateMutability !== 'view';
-      const invoker = getInvokeElement(method, { id });
+    const inputElements = inputs.map((input) => {
+      const key = input.name;
+      return { key, [key]: formatHtml(getInputElement(input, { id })) };
+    });
+    const outputElements = outputs.map((output, index) => {
+      const key = output.name;
+      return { key, [key]: formatHtml(getOutputElement(output, { id, isTransaction, index })) };
+    });
 
-      const inputElements = inputs.map((input) => {
-        const key = input.name ? input.name : 'anonymous';
-        return { key, [key]: formatHtml(getInputElement(input, { id })) };
-      });
-      const outputElements = outputs.map((output, index) => {
-        const key = output.name ? output.name : 'anonymous';
-        return { key, [key]: formatHtml(getOutputElement(output, { id, isTransaction, index })) };
-      });
+    const autoInvoke = inputs.length === 0 ? 'true' : 'false';
+    const children = formatHtml([...getHtmlFromIO(inputElements), ...getHtmlFromIO(outputElements), invoker].join(''));
 
-      const autoInvoke = inputs.length === 0 ? 'true' : 'false';
-      const children = formatHtml([...getHtmlFromIO(inputElements), ...getHtmlFromIO(outputElements), invoker].join(''));
+    const featureElement = getFeatureElement(method, { id, children, autoInvoke, contractName });
 
-      const featureElement = getFeatureElement(method, { id, children, autoInvoke, contractName });
-
-      return {
-        methodName: name,
-        html: wrapIntoTags(featureElement),
-        metadata: { inputs: inputElements, outputs: outputElements, invoke: invoker },
-      };
-
+    return {
+      methodName: name,
+      html: wrapIntoTags(featureElement),
+      metadata: { inputs: inputElements, outputs: outputElements, invoke: invoker },
+    };
   });
-
+};
 const wrapIntoTags = (html, wrapperTag = '', title = '') => {
   return wrapperTag ? `<${wrapperTag}>${title ? `<h2>${title}</h2>` : ''}${html}</${wrapperTag}>` : `${html}`;
 };
@@ -153,7 +149,8 @@ module.exports = {
 };
 
 // test
-//const { abi } = require('../mocks/abi');
+// const { abi } = require('../mocks/abi');
+// console.log(generateHtmlPieces(abi, 'a'));
 // console.log(
 //   JSON.stringify({
 //     abis: [
