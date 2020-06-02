22d7069d871ca2d447619f0bf3b76974d03f05fc
Dennison Bertram <dennison@dennisonbertram.com>
2020-06-02 11:45:00 -0400

    👌 IMPROVE:  Refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const prettier = require('prettier');
 const chalk = require('chalk');
-const log = console.log;
 
 // lib
 const { generateUniqueId } = require('../lib/id');
@@ -30,12 +29,12 @@ const getFeatureElement = (
   </header>
 `;
 
-const getInputElement = ({ name = '', type }, { id = '' , key, hasMoreThanOneAnonymousInput}) => {
-  
+const getInputElement = ({ name = '', type }, { id = '', key, hasMoreThanOneAnonymousInput }) => {
+
   const getInputName = () => {
-    let inputName = name 
-    if(!name) inputName = '$true'
-    if(hasMoreThanOneAnonymousInput) inputName = `[${key}]`
+    let inputName = name
+    if (!name) inputName = '$true'
+    if (hasMoreThanOneAnonymousInput) inputName = `[${key}]`
     return inputName
   }
 
@@ -49,11 +48,10 @@ Input name: <i>${name ? name : "anonymous input"}</i>
     data-dh-property-input-name="${getInputName()}"
   />
   </section>
-`)};
+`)
+};
 
 const getOutputElement = ({ name = '' }, { id = '', index, isTransaction }) => {
-
-  // log(chalk.red('Hello'))
 
   return (`
   <div
@@ -88,67 +86,68 @@ const getScriptTag = (projectId) => `
     </style>
 `;
 
-const getHeaderTag = (projectDescription, projectImage, projectName) => `    <header>
-<nav>
-    <img alt="Logo" src="${projectImage ? projectImage : "https://dd7tel2830j4w.cloudfront.net/f1588198682293x257424398827881060/Artboard%201.svg"}" height="70">
-    <ul>
-        <li>Menu Item 1</li>  
-        <li><a href="#">Menu Item 2</a></li>
-        <li><a href="#">Dropdown Menu Item</a>
-            <ul>
-                <li><a href="#">Sublink with a long name</a></li>
-                <li><a href="#">Short sublink</a></li>
-            </ul>
-        </li>
-    </ul>
-</nav>
-<h1>${projectName ? projectName : "My Sample Project"}</h1>
-<p><mark>Powered by <a href="https://www.dapphero.io">DappHero.io</a></mark></p>
-<p>${projectDescription ? projectDescription : "My sample project description"}</p>
-<br>
-<p>  <button
-data-dh-feature="network"
-data-dh-property-enable="true"
->
-Enable Web3
-</button></p>
+const getHeaderTag = (projectDescription, projectImage, projectName) => `
+<header>
+  <nav>
+      <img alt="Logo" src="${projectImage ? projectImage : "https://dd7tel2830j4w.cloudfront.net/f1588198682293x257424398827881060/Artboard%201.svg"}" height="70">
+      <ul>
+          <li>Menu Item 1</li>  
+          <li><a href="#">Menu Item 2</a></li>
+          <li><a href="#">Dropdown Menu Item</a>
+              <ul>
+                  <li><a href="#">Sublink with a long name</a></li>
+                  <li><a href="#">Short sublink</a></li>
+              </ul>
+          </li>
+      </ul>
+  </nav>
+  <h1>${projectName ? projectName : "My Sample Project"}</h1>
+  <p>
+    <mark>
+      Powered by <a href="https://www.dapphero.io">DappHero.io</a>
+    </mark>
+  </p>
+  <p>${projectDescription ? projectDescription : "My sample project description"}</p>
+  <br>
+  <p>
+    <button data-dh-feature="network" data-dh-property-enable="true">
+      Enable Web3
+    </button>
+  </p>
 </header>`
 
 const getWeb3Tag = () => `
-  <button
-    data-dh-feature="network"
-    data-dh-property-enable="true"
-  >
+  <button data-dh-feature="network" data-dh-property-enable="true">
     Enable Web3
   </button>
 `;
 
 const getCustomJavascriptTag = (networkId, networkName) => `
 <script>
-document.addEventListener(
-  "dappHeroConfigLoaded",
-  ({ detail: dappHero }) => {
-    // Inside here you can listen to any event you want
-    console.log("DappHero Has Loaded!");
+  document.addEventListener(
+    "dappHeroConfigLoaded",
+    ({ detail: dappHero }) => {
+      // Inside here you can listen to any event you want
+      console.log("DappHero Has Loaded!");
 
-    // For projects with auto-invoked method, you will want to filter on events
-    // dappHero.listenToTransactionStatusChange(data => {
-    //   console.log("Listening to transtaction status change", data);
-    // });
+      // For projects with auto-invoked method, you will want to filter on events
+      // dappHero.listenToTransactionStatusChange(data => {
+      //   console.log("Listening to transtaction status change", data);
+      // });
 
-    // This is how you can listen to any events your smart contract emits
-    dappHero.listenToSmartContractBlockchainEvent(data => {
-      console.log("The blockChain Events: ", data)
-    })
+      // This is how you can listen to any events your smart contract emits
+      dappHero.listenToSmartContractBlockchainEvent(data => {
+        console.log("The blockChain Events: ", data)
+      })
 
-    
-    ${networkId && networkName ? `
-    //This is an example method for alerting your users they are on the wrong network
-    //if(window.dappHero.provider.chainId !== ${networkId}){
-    //  alert("Wrong network! You should be on ${networkName}")
-    //}` : 
-    ""}
-  });
+      
+      ${networkId && networkName ? `
+      //This is an example method for alerting your users they are on the wrong network
+      //if(window.dappHero.provider.chainId !== ${networkId}){
+      //  alert("Wrong network! You should be on ${networkName}")
+      //}` :
+      ""}
+    });
 </script>`
 
 const getHtmlFromPieces = (pieces) => pieces.map(({ html }) => html);
@@ -173,11 +172,11 @@ const generateHtmlPieces = (abi = [], contractName) => {
     }
     // console.log("inputs: ", newInputs)
 
-    const hasMoreThanOneAnonymousInput = newInputs.map(({name}) => name).filter((name) => name === '').length > 1
-    
+    const hasMoreThanOneAnonymousInput = newInputs.map(({ name }) => name).filter((name) => name === '').length > 1
+
     const inputElements = newInputs.map((input, index) => {
       const key = input.name || index
-      
+
       return { key, [key]: formatHtml(getInputElement(input, { id, key, hasMoreThanOneAnonymousInput })) };
     });
     const outputElements = outputs.map((output, index) => {
@@ -229,7 +228,7 @@ const getEntireHtml = ({ abis, projectId, projectDescription, projectImage, proj
     .join('\n');
 
   // const web3Tag = getWeb3Tag();
-  const headerTag = getHeaderTag(projectDescription, projectImage, projectName );
+  const headerTag = getHeaderTag(projectDescription, projectImage, projectName);
   const customJavascript = getCustomJavascriptTag(projectNetworkId, projectNetworkName);
   const scriptTag = getScriptTag(projectId);
   const html = createHtmlTemplate(`${headerTag}${tags}${scriptTag}${customJavascript}`);
